6b5a0e1d56c78f7715db3952416c8529b0ff061c
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-24 12:40:25 -0500

    implemented init.php

---
+++ backend/init.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    include_once 'classes/Database.php';
+
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "blogger");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("BASE_URL", "http://localhost/blogger/");
+?>
--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
     include_once 'partials/headers.php';
 
-    include_once 'backend/classes/Database.php';
+    include_once 'backend/init.php';
     //header("location: login.php");
+
+    echo DB_HOST;
+    echo '<br>';
+    echo DB_NAME;
+    echo '<br>';
+    echo BASE_URL;
     
 ?>
     
