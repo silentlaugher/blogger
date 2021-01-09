f1cd839687218fe4eba01ffb8662be155162097a
Christopher Edynak <christopher1edynak@gmail.com>
2021-01-09 13:00:57 -0500

    implemented method to update password

--- frontend/assets/js/editProfile.js
+++ frontend/assets/js/editProfile.js
@@ -38,3 +38,63 @@ formBtn.addEventListener("click", function(event){
 		}	
 	}
 });
+
+document.querySelector('#editProfile').addEventListener("change", function(event){
+    var regex = /(\.jpg|\.jpeg|\.png|\.zip)$/i;
+    if(!regex.exec(this.value)){
+        alert("Only '.jpeg','.jpg','.png', formats are allowed");
+        this.value = '';
+        return false;
+    }else{
+        if(this.files && this.files[0]){
+            var reader  = new FileReader();
+            reader.onload = function(event){
+                document.querySelector("#editProfileImage").src = event.target.result;
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    }
+});
+
+var savePassBtn = document.querySelector("#savePassBtn");
+
+savePassBtn.addEventListener("click", function(event){
+var currPass = document.querySelector("#editCurPass").value;
+var newPass = document.querySelector("#editNewPass").value;
+var newPassRe = document.querySelector("#editNewPassAgain").value;
+
+if(currPass && newPass && newPassRe !== ""){
+    if(newPass.length && newPassRe.length < 6){
+        alert("Your password is too short");
+    }else{
+        if(newPass !== newPassRe){
+            alert("Your new password does not match!");	
+        }else{
+            var blogID = this.dataset.blog;
+            var formData = new FormData();
+            formData.append("blogID", blogID);
+            formData.append("currPass", currPass);
+            formData.append("newPass", newPass);
+            formData.append("newPassRe", newPassRe);
+
+            var httpRequest = new XMLHttpRequest();
+
+            if(httpRequest){
+                httpRequest.open('POST', 'http://localhost/blogger/backend/ajax/changePassword.php', true);
+                httpRequest.onreadystatechange = function(){
+                    if(this.readyState === 4 && this.status === 200){
+                        if(this.responseText.length != 0){
+                            alert(this.responseText);
+                        }
+                        location.reload(true);
+                    }
+                }
+
+                httpRequest.send(formData);
+            }
+        }
+    }
+}else{
+    alert("Enter your Password to update!");
+}
+});
